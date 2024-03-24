bb049f6f30937fd654b9d9bc24b01221c4b540ad
Tudorache Leonard Valentin <valileo2000@gmail.com>
2024-03-24 23:47:57 +0200

    fix: replaced message from useCallback to useMemo to not compute on each rerender

--- assets/pages/search.tsx
+++ assets/pages/search.tsx
@@ -1,12 +1,14 @@
 import React, {useEffect, useRef} from "react";
 import WebSocketContextProvider from "../context/WebSocketContextProvider";
 import Search from "../components/Search";
+import SettingsPanel from "../components/SettingsPanel";
 
 export default function CallToActionWithAnnotation() {
 
     return (
         <WebSocketContextProvider>
             <Search/>
+            <SettingsPanel/>
         </WebSocketContextProvider>
     );
 }
