4a6c3b365424d158b087f71f0ab2614940834472
Tudorache Leonard Valentin <valileo2000@gmail.com>
2024-04-05 23:24:47 +0300

    feat: persist states

--- assets/stores/websocketStore.ts
+++ assets/stores/websocketStore.ts
@@ -1,5 +1,5 @@
 import {createStore} from 'zustand'
-import {subscribeWithSelector} from "zustand/middleware";
+import {createJSONStorage, persist, subscribeWithSelector} from "zustand/middleware";
 import {createContext} from "react";
 import createWs from "../functions/createWs";
 import {Results} from "../types/ws/results/Results";
@@ -38,96 +38,113 @@ export interface WebsocketStoreState {
     }
 }
 
+const indexedDb = window.indexedDB.open("HyperSearchDB", 3);
 /**
  * Store for the websocket connection.
  */
 export const createWebsocketStore = () => {
 
     const store = createStore<WebsocketStoreState>()(
-        subscribeWithSelector((set, get) => ({
-            states: {
-                state: WebsocketState.Uninitialized,
-                socket: null,
-                pastMessages: [],
-                pastResults: [],
-                mappedResults: {},
-                mappedMessages: {}
-            },
-            actions: {
-                reconnect: () => set((_store) => {
-                    setTimeout(() => {
-                        createWs(store)
-                    }, 3000)
+        subscribeWithSelector(persist((set, get) => ({
+                    states: {
+                        state: WebsocketState.Uninitialized,
+                        socket: null,
+                        pastMessages: [],
+                        pastResults: [],
+                        mappedResults: {},
+                        mappedMessages: {}
+                    },
+                    actions: {
+                        reconnect: () => set((_store) => {
+                            setTimeout(() => {
+                                createWs(store)
+                            }, 3000)
 
-                    return {
-                        states: {
-                            ..._store.states,
-                            state: WebsocketState.Connecting
-                        }
-                    };
-                }),
-                /**
-                 * Send a message to the websocket server
-                 * @param message
-                 */
-                sendMessage: <T extends Payloads>(message: Message<T>) => set((store) => {
+                            return {
+                                states: {
+                                    ..._store.states,
+                                    state: WebsocketState.Connecting
+                                }
+                            };
+                        }),
+                        /**
+                         * Send a message to the websocket server
+                         * @param message
+                         */
+                        sendMessage: <T extends Payloads>(message: Message<T>) => set((store) => {
 
-                    if (store.states.socket === null) {
-                        throw new Error("Socket not initialized")
-                    }
-
-                    store.states.socket.send(JSON.stringify(message))
-
-                    return {
-                        states: {
-                            ...store.states,
-                            mappedMessages: {
-                                ...store.states.mappedMessages,
-                                [message.type]: message.payload
-                            },
-                            pastMessages: [...store.states.pastMessages, {
-                                id: store.states.pastMessages.length,
-                                ...message
-                            }],
-                        }
-                    };
-                }),
-                /**
-                 * Save the result from the websocket message to the store
-                 * @param result
-                 */
-                saveResult:
-                    (result) => set((store) => (
-                        {
-                            states: {
-                                ...store.states,
-                                mappedResults: {
-                                    ...store.states.mappedResults,
-                                    [result.type]: result
-                                },
-                                pastResults: [...store.states.pastResults, {
-                                    id: store.states.pastResults.length,
-                                    ...result
-                                }],
+                            if (store.states.socket === null) {
+                                throw new Error("Socket not initialized")
                             }
-                        }
-                    )),
-            },
-            _internals: {
-                /**
-                 * Set the socket status
-                 * @param state
-                 */
-                _setSocketStatus: (state: WebsocketState) => set((store) => (
-                    {
-                        states: {
-                            ...store.states,
-                            state: state
-                        }
+
+                            store.states.socket.send(JSON.stringify(message))
+
+                            return {
+                                states: {
+                                    ...store.states,
+                                    mappedMessages: {
+                                        ...store.states.mappedMessages,
+                                        [message.type]: message.payload
+                                    },
+                                    pastMessages: [...store.states.pastMessages, {
+                                        id: store.states.pastMessages.length,
+                                        ...message
+                                    }],
+                                }
+                            };
+                        }),
+                        /**
+                         * Save the result from the websocket message to the store
+                         * @param result
+                         */
+                        saveResult:
+                            (result) => set((store) => (
+                                {
+                                    states: {
+                                        ...store.states,
+                                        mappedResults: {
+                                            ...store.states.mappedResults,
+                                            [result.type]: result
+                                        },
+                                        pastResults: [...store.states.pastResults, {
+                                            id: store.states.pastResults.length,
+                                            ...result
+                                        }],
+                                    }
+                                }
+                            )),
+                    },
+                    _internals: {
+                        /**
+                         * Set the socket status
+                         * @param state
+                         */
+                        _setSocketStatus: (state: WebsocketState) => set((store) => (
+                            {
+                                states: {
+                                    ...store.states,
+                                    state: state
+                                }
+                            }
+                        )),
                     }
-                )),
-            }
-        }))
+                }),
+                {
+                    name: 'websocket-storage',
+                    storage: createJSONStorage(() => localStorage),
+                    partialize: (state) => ({
+                        states: {
+                            state: WebsocketState.Uninitialized,
+                            socket: null,
+                            pastResults: [],
+                            mappedResults: {},
+                            mappedMessages: {},
+                            pastMessages: state.states.pastMessages.filter((message) => message.type === MessageTypes.SearchQuery),
+                        }
+                    }),
+                },
+            ),
+        )
     );
 
     // Create the websocket connection
